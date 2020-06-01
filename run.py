88d003eb63fcfaccde2a379d273ec16766ce0e01
vishkid84 <vishalittr@gmail.com>
2020-06-01 17:06:54 +0000

    Added index.html and redirect

--- run.py
+++ run.py
@@ -1,30 +1,36 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 
 def add_messages(username, message):
-    """Add messages to the 'messages' list"""
-    messages.append("{}: {}".format(username, message))
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 
-def get_all_messages():
-    """Get all of the messages and separate them with a 'br'"""
-    return "<br>".join(messages)
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use: /USERNAME/MESSAGE"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
 
 
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
 
 
 @app.route("/<username>/<message>")
